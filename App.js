d6bd9e7e4734996c5a43d9a47d5bc2edeb86239f
Asanka Weligamage <asanka.weligamage@gmail.com>
2022-08-19 19:57:56 +0530

    inittial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
